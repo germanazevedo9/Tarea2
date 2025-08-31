b6ee66467cf90eb411da43a033ba704c52624433
German <tu@email.com>
2025-08-31 20:23:38 -0300

    trabajando un poco en el primer modulo

--- src/socio.cpp
+++ src/socio.cpp
@@ -1,13 +1,34 @@
 #include "../include/socio.h"
 
 struct rep_socio {
+    int ciSocio;
+    char nombreSocio[MAX_NOMBRE_SOCIO];
+    char apellidoSocio[MAX_APELLIDO_SOCIO];
+    TFecha fechaAlta;
+    struct rep_generos{
+        int genfavs[MAX_GENEROS_FAVORITOS];
+        int tope;
+    }generos;
 };
  
 TSocio crearTSocio(int ci, const char nombre[MAX_NOMBRE_SOCIO], const char apellido[MAX_APELLIDO_SOCIO], nat diaAlta, nat mesAlta, nat anioAlta){
-    return NULL;
+    TSocio nuevoSocio = new rep_socio;
+    nuevoSocio->ciSocio = ci;
+    strcpy(nuevoSocio->nombreSocio,nombre);
+    strcpy(nuevoSocio->apellidoSocio,apellido);
+    nuevoSocio->fechaAlta = crearTFecha(diaAlta,mesAlta,anioAlta);
+    nuevoSocio->generos.tope=0;
+    return nuevoSocio;
 }
 
 void imprimirTSocio(TSocio socio){
+    printf("Socio %d %s %s\n", socio->ciSocio,":",socio->nombreSocio,socio->apellidoSocio);
+    printf("Fecha de alta: " );
+    imprimirTFecha(socio->fechaAlta);
+    printf("Géneros favoritos: ");
+    for (int i=0;i<socio->generos.tope;i++){
+        printf("%d\n",socio->generos.genfavs[i]);
+    };
 }
 
 void liberarTSocio(TSocio &socio){
