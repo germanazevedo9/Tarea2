4cee061ed65f6400716900cee64933dce0e8d370
German <tu@email.com>
2025-08-31 21:34:05 -0300

    parte 1 del modulo 1 hecha y funcionando correctamente

--- src/socio.cpp
+++ src/socio.cpp
@@ -20,34 +20,44 @@ TSocio crearTSocio(int ci, const char nombre[MAX_NOMBRE_SOCIO], const char apell
     nuevoSocio->generos.tope=0;
     return nuevoSocio;
 }
-
+void imprimirGenerosDelSocio(TSocio socio){
+    for (int i=0;i<socio ->generos.tope;i++){
+        printf("%d\n",socio->generos.genfavs[i]);
+    };
+    printf("\n");
+};
 void imprimirTSocio(TSocio socio){
-    printf("Socio %d %s %s\n", socio->ciSocio,":",socio->nombreSocio,socio->apellidoSocio);
+    printf("Socio %d: %s %s\n", socio->ciSocio,socio->nombreSocio,socio->apellidoSocio);
     printf("Fecha de alta: " );
     imprimirTFecha(socio->fechaAlta);
     printf("Géneros favoritos: ");
-    for (int i=0;i<socio->generos.tope;i++){
-        printf("%d\n",socio->generos.genfavs[i]);
-    };
+    imprimirGenerosDelSocio(socio);
 }
 
 void liberarTSocio(TSocio &socio){
+    liberarTFecha(socio->fechaAlta);
+    delete socio;
+    socio = NULL;
 }
 
 int ciTSocio(TSocio socio){
-    return 0;
+    int copiaCi = socio->ciSocio;
+    return copiaCi;
 }
 
 char* nombreTSocio(TSocio socio){
-    return NULL;
+    char* copiaNombre = socio->nombreSocio; 
+    return copiaNombre;
 }
 
 char* apellidoTSocio(TSocio socio){
-    return NULL;
+    char* copiaapellido = socio->apellidoSocio; 
+    return copiaapellido;
 }
 
 TFecha fechaAltaTSocio(TSocio socio){
-    return NULL;
+    TFecha copia = socio->fechaAlta;
+    return copia;
 }
 
 void agregarGeneroFavoritoTSocio(TSocio &socio, int idGenero){
